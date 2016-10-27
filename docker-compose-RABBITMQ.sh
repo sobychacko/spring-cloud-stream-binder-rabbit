5dc73e5158d5d0c11cd95c7cfcdb6a3da7aedf2a
Soby Chacko <schacko@pivotal.io>
2016-10-27 14:57:14 -0400

    rabbitmq docker compose start in scst binder

--- docker-compose-RABBITMQ.sh
+++ docker-compose-RABBITMQ.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SYSTEM_PROPS="-DRABBIT_HOST=${HEALTH_HOST} -Dspring.rabbitmq.port=9672"
+#SYSTEM_PROPS="-DRABBIT_HOST=${HEALTH_HOST} -Dspring.rabbitmq.port=9672"
 
-dockerComposeFile="docker-compose-${WHAT_TO_TEST}.yml"
+dockerComposeFile="docker-compose-RABBITMQ.yml"
 docker-compose -f $dockerComposeFile kill
 docker-compose -f $dockerComposeFile build
 
@@ -13,6 +13,11 @@ fi
 
 READY_FOR_TESTS="no"
 PORT_TO_CHECK=9672
+
+HEALTH_HOST="${HEALTH_HOST:-127.0.0.1}"
+WAIT_TIME="${WAIT_TIME:-5}"
+RETRIES="${RETRIES:-70}"
+
 echo "Waiting for RabbitMQ to boot for [$(( WAIT_TIME * RETRIES ))] seconds"
 netcat_port $PORT_TO_CHECK && READY_FOR_TESTS="yes"
 
@@ -20,3 +25,14 @@ if [[ "${READY_FOR_TESTS}" == "no" ]] ; then
     echo "RabbitMQ failed to start..."
     exit 1
 fi
+
+function netcat_port() {
+    local PASSED_HOST="${2:-$HEALTH_HOST}"
+    local READY_FOR_TESTS=1
+    for i in $( seq 1 "${RETRIES}" ); do
+        sleep "${WAIT_TIME}"
+        nc -v -w 1 ${PASSED_HOST} $1 && READY_FOR_TESTS=0 && break
+        echo "Fail #$i/${RETRIES}... will try again in [${WAIT_TIME}] seconds"
+    done
+    return ${READY_FOR_TESTS}
+}
